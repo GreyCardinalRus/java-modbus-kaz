5dd34364ac6c039a4fc22a32af63134c862c31e6
grey.c.rus <grey.c.rus@localhost>
2013-10-04 12:44:23 +0400

    ??????????? ????? , ????????? ?????????? ??? ?????? ? ?????? ????? lib ????? ?????? ?? 3 ?????

--- src/mvc/views/PenFormView.java
+++ src/mvc/views/PenFormView.java
@@ -24,9 +24,9 @@ import jssc.SerialPort;
 import jssc.SerialPortList;
 import javax.swing.JSpinner;
 import javax.swing.JMenu;
-import javax.swing.JMenuItem;
+//import javax.swing.JMenuItem;
 import javax.swing.JList;
-import javax.swing.AbstractListModel;
+//import javax.swing.AbstractListModel;
 import javax.swing.JScrollPane;
 
 import net.wimpi.modbus.Modbus;
@@ -37,7 +37,7 @@ import net.wimpi.modbus.io.ModbusSerialTransaction;
 import net.wimpi.modbus.msg.DA555ReadID;
 import net.wimpi.modbus.msg.ModbusRequest;
 import net.wimpi.modbus.msg.ModbusResponse;
-import net.wimpi.modbus.msg.ReadInputRegistersRequest;
+//import net.wimpi.modbus.msg.ReadInputRegistersRequest;
 import net.wimpi.modbus.msg.ReadMultipleRegistersRequest;
 import net.wimpi.modbus.msg.WriteMultipleRegistersRequest;
 import net.wimpi.modbus.net.SerialConnection;
@@ -51,11 +51,13 @@ import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartPanel;
 import org.jfree.chart.JFreeChart;
 import org.jfree.data.category.DefaultCategoryDataset;
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.SpinnerNumberModel;
 
 public class PenFormView {
 
 	final int unitid = 1;
-	int imax=100;
+	//int imax=100;
 	private JFrame frame;
 	JComboBox<String> comboBoxPort0 = new JComboBox<String>();
 	JComboBox<String> comboBoxPort1 = new JComboBox<String>();
@@ -132,8 +134,8 @@ public class PenFormView {
 				dataset.addValue(Math.sin(isSimulation*.1+Math.random()*0.1), "Series sin", " " + (isSimulation-1));
 				dataset.addValue(Math.cos(isSimulation*.1+Math.random()*0.2-0.1), "Series cos", " " + (isSimulation-1));
 				dataset.addValue(Math.cos(isSimulation*.1+Math.PI/10), "Series Ideal", " " + (isSimulation-1));
-				if(isSimulation>=imax){
-					dataset.removeColumn(" " + (isSimulation-imax));
+				if(isSimulation>=((Integer) windowChartWidth.getValue())){
+					dataset.removeColumn(" " + (isSimulation-( (Integer) windowChartWidth.getValue())));
 					//System.out.println("Delete "+i);
 				}
 				listModel.addElement("Word " + isSimulation + "="
@@ -141,14 +143,8 @@ public class PenFormView {
 				int index = listModel.size() - 1;
 				listOutput.setSelectedIndex(index);
 				listOutput.ensureIndexIsVisible(index);
+				if(listModel.size()>20) listModel.clear();
 
-//				try {
-//					Thread.sleep(100); // спать 1000 милисекунд.
-//				} catch (Exception e) {
-//					System.out
-//							.println("Что-то пошло не так, но бог здесь не причём, ибо его нет. Это просто какой-то косяк, который можно объяснить.");
-//				}
-			//}
 				isSimulation++;
 		} else {
 			// TODO Читаем регистры
@@ -214,6 +210,10 @@ public class PenFormView {
 	};
 	private final JPanel SettingsPanel = new JPanel();
 	private final JPanel IndicatorsPanel = new JPanel();
+	private final JComboBox<String> comboBoxSpeed2 = new JComboBox<String>();
+	private final JComboBox<String> comboBoxPar2 = new JComboBox<String>();
+	private final JComboBox<String> comboBoxStopBits2 = new JComboBox<String>();
+	private JSpinner windowChartWidth;
 
 	private void initialize() {
 		frame = new JFrame();
@@ -244,94 +244,123 @@ public class PenFormView {
 		tabbedPane.addTab("Лог", null, LogPanel, null);
 		LogPanel.setLayout(new BorderLayout(0, 0));
 		LogPanel.add(listOutput, BorderLayout.NORTH);
-
-
-		tabbedPane.addTab("Настройки", null, SettingsPanel, null);
-		SettingsPanel.setLayout(null);
-
-		JLabel lblLpw = new JLabel("LPW-305");
-		lblLpw.setBounds(190, 9, 60, 15);
-		SettingsPanel.add(lblLpw);
-
-		JLabel label = new JLabel("Четность");
-		label.setBounds(48, 132, 65, 15);
-		SettingsPanel.add(label);
-		comboBoxStopBits0.setBounds(124, 172, 137, 24);
-		SettingsPanel.add(comboBoxStopBits0);
-		comboBoxStopBits0
-				.setModel(new javax.swing.DefaultComboBoxModel<String>(
-						new String[] { "1", "1.5", "2" }));
-
-		JLabel lblNewLabel_1 = new JLabel("StopBits");
-		lblNewLabel_1.setBounds(34, 167, 60, 15);
-		SettingsPanel.add(lblNewLabel_1);
-
-		JLabel lblNewLabel = new JLabel("Port");
-		lblNewLabel.setBounds(49, 45, 30, 15);
-		SettingsPanel.add(lblNewLabel);
-		comboBoxPort0.setBounds(124, 40, 168, 24);
-		SettingsPanel.add(comboBoxPort0);
-
-		JLabel lblSpeed = new JLabel("Speed");
-		lblSpeed.setBounds(49, 92, 45, 15);
-		SettingsPanel.add(lblSpeed);
-		comboBoxSpeed0.setBounds(124, 87, 168, 24);
-		SettingsPanel.add(comboBoxSpeed0);
-		comboBoxSpeed0.setModel(new javax.swing.DefaultComboBoxModel<String>(
-				new String[] { "110", "300", "600", "1200", "4800", "9600",
-						"14400", "19200", "38400", "57600", "115200" }));
-		comboBoxSpeed0.setSelectedIndex(7);
-		comboBoxPort1.setBounds(330, 40, 143, 24);
-		SettingsPanel.add(comboBoxPort1);
-		comboBoxPar0.setBounds(124, 123, 168, 24);
-		SettingsPanel.add(comboBoxPar0);
-		comboBoxPar0.setModel(new javax.swing.DefaultComboBoxModel<String>(
-				new String[] { "None", "Odd", "Even", "Mark", "Space" }));
-
-		JLabel lblNewLabel_2 = new JLabel("Уставка");
-		lblNewLabel_2.setBounds(31, 269, 67, 15);
-		SettingsPanel.add(lblNewLabel_2);
-		Ustavka.setBounds(110, 264, 79, 20);
-		SettingsPanel.add(Ustavka);
-		Ustavka.setValue(5);
-
-		JLabel lblNewLabel_3 = new JLabel("Шаг настройки");
-		lblNewLabel_3.setBounds(34, 308, 107, 15);
-		SettingsPanel.add(lblNewLabel_3);
-		StepSetting.setBounds(160, 310, 73, 20);
-		SettingsPanel.add(StepSetting);
-		StepSetting.setValue(100);
-		label_1.setBounds(322, 269, 79, 15);
-		SettingsPanel.add(label_1);
-		Gisterezis.setBounds(475, 266, 78, 22);
-		SettingsPanel.add(Gisterezis);
-		Gisterezis.setValue(1000);
-		lblNewLabel_4.setBounds(322, 312, 132, 15);
-		SettingsPanel.add(lblNewLabel_4);
-		ajustPeriod.setBounds(478, 308, 75, 24);
-		SettingsPanel.add(ajustPeriod);
-		ajustPeriod.setValue(100);
-		comboBoxPar1.setBounds(330, 127, 137, 24);
-		SettingsPanel.add(comboBoxPar1);
-		comboBoxPar1.setModel(new javax.swing.DefaultComboBoxModel<String>(
-				new String[] { "None", "Odd", "Even", "Mark", "Space" }));
-		comboBoxStopBits1.setBounds(340, 172, 132, 24);
-		SettingsPanel.add(comboBoxStopBits1);
-		comboBoxStopBits1
-				.setModel(new javax.swing.DefaultComboBoxModel<String>(
-						new String[] { "1", "1.5", "2" }));
-		comboBoxSpeed1.setBounds(326, 87, 143, 24);
-		SettingsPanel.add(comboBoxSpeed1);
-		comboBoxSpeed1.setModel(new javax.swing.DefaultComboBoxModel<String>(
-				new String[] { "110", "300", "600", "1200", "4800", "9600",
-						"14400", "19200", "38400", "57600", "115200" }));
-		comboBoxSpeed1.setSelectedIndex(5);
-
-		JLabel lblEnergyform = new JLabel("EnergyForm");
-		lblEnergyform.setBounds(339, 9, 160, 15);
-		SettingsPanel.add(lblEnergyform);
 		JScrollPane scrollPane = new JScrollPane();
 		tabbedPane.addTab("New tab", null, scrollPane, null);
+		
+		
+				tabbedPane.addTab("Settings", null, SettingsPanel, null);
+				SettingsPanel.setLayout(null);
+				
+						JLabel lblLpw = new JLabel("LPW-305");
+						lblLpw.setBounds(154, 9, 60, 15);
+						SettingsPanel.add(lblLpw);
+						
+								JLabel label = new JLabel("Четность");
+								label.setBounds(34, 119, 65, 15);
+								SettingsPanel.add(label);
+								comboBoxStopBits0.setBounds(96, 162, 137, 24);
+								SettingsPanel.add(comboBoxStopBits0);
+								comboBoxStopBits0
+										.setModel(new javax.swing.DefaultComboBoxModel<String>(
+												new String[] { "1", "1.5", "2" }));
+								
+										JLabel lblNewLabel_1 = new JLabel("StopBits");
+										lblNewLabel_1.setBounds(34, 167, 60, 15);
+										SettingsPanel.add(lblNewLabel_1);
+										
+												JLabel lblNewLabel = new JLabel("Port");
+												lblNewLabel.setBounds(49, 45, 45, 19);
+												SettingsPanel.add(lblNewLabel);
+												comboBoxPort0.setBounds(96, 42, 168, 24);
+												SettingsPanel.add(comboBoxPort0);
+												
+														JLabel lblSpeed = new JLabel("Speed");
+														lblSpeed.setBounds(49, 92, 45, 15);
+														SettingsPanel.add(lblSpeed);
+														comboBoxSpeed0.setBounds(96, 78, 168, 24);
+														SettingsPanel.add(comboBoxSpeed0);
+														comboBoxSpeed0.setModel(new javax.swing.DefaultComboBoxModel<String>(
+																new String[] { "110", "300", "600", "1200", "4800", "9600",
+																		"14400", "19200", "38400", "57600", "115200" }));
+														comboBoxSpeed0.setSelectedIndex(7);
+														comboBoxPort1.setBounds(289, 42, 143, 24);
+														SettingsPanel.add(comboBoxPort1);
+														comboBoxPar0.setBounds(96, 114, 168, 24);
+														SettingsPanel.add(comboBoxPar0);
+														comboBoxPar0.setModel(new javax.swing.DefaultComboBoxModel<String>(
+																new String[] { "None", "Odd", "Even", "Mark", "Space" }));
+														
+																JLabel lblNewLabel_2 = new JLabel("Уставка");
+																lblNewLabel_2.setBounds(31, 269, 67, 15);
+																SettingsPanel.add(lblNewLabel_2);
+																Ustavka.setBounds(110, 264, 79, 20);
+																SettingsPanel.add(Ustavka);
+																Ustavka.setValue(5);
+																
+																		JLabel lblNewLabel_3 = new JLabel("Шаг настройки");
+																		lblNewLabel_3.setBounds(34, 308, 107, 15);
+																		SettingsPanel.add(lblNewLabel_3);
+																		StepSetting.setBounds(160, 310, 73, 20);
+																		SettingsPanel.add(StepSetting);
+																		StepSetting.setValue(100);
+																		label_1.setBounds(322, 269, 79, 15);
+																		SettingsPanel.add(label_1);
+																		Gisterezis.setBounds(475, 266, 78, 22);
+																		SettingsPanel.add(Gisterezis);
+																		Gisterezis.setValue(1000);
+																		lblNewLabel_4.setBounds(322, 312, 132, 15);
+																		SettingsPanel.add(lblNewLabel_4);
+																		ajustPeriod.setBounds(478, 308, 75, 24);
+																		SettingsPanel.add(ajustPeriod);
+																		ajustPeriod.setValue(100);
+																		comboBoxPar1.setBounds(289, 119, 137, 24);
+																		SettingsPanel.add(comboBoxPar1);
+																		comboBoxPar1.setModel(new DefaultComboBoxModel(new String[] {"None", "Odd", "Even", "Mark", "Space"}));
+																		comboBoxStopBits1.setBounds(289, 162, 132, 24);
+																		SettingsPanel.add(comboBoxStopBits1);
+																		comboBoxStopBits1
+																				.setModel(new javax.swing.DefaultComboBoxModel<String>(
+																						new String[] { "1", "1.5", "2" }));
+																		comboBoxSpeed1.setBounds(299, 87, 143, 24);
+																		SettingsPanel.add(comboBoxSpeed1);
+																		comboBoxSpeed1.setModel(new javax.swing.DefaultComboBoxModel<String>(
+																				new String[] { "110", "300", "600", "1200", "4800", "9600",
+																						"14400", "19200", "38400", "57600", "115200" }));
+																		comboBoxSpeed1.setSelectedIndex(7);
+																		
+																				JLabel lblEnergyform = new JLabel("EnergyForm");
+																				lblEnergyform.setBounds(289, 9, 160, 15);
+																				SettingsPanel.add(lblEnergyform);
+																				
+																				JComboBox<String> comboBoxPort2 = new JComboBox<String>();
+																				comboBoxPort2.setBounds(454, 42, 143, 24);
+																				SettingsPanel.add(comboBoxPort2);
+																				comboBoxSpeed2.setModel(new DefaultComboBoxModel(new String[] {"110", "300", "600", "1200", "4800", "9600", "14400", "19200", "38400", "57600", "115200"}));
+																				comboBoxSpeed2.setSelectedIndex(7);
+																				comboBoxSpeed2.setBounds(464, 87, 143, 24);
+																				
+																				SettingsPanel.add(comboBoxSpeed2);
+																				comboBoxPar2.setModel(new DefaultComboBoxModel(new String[] {"None", "Odd", "Even", "Mark", "Space"}));
+																				comboBoxPar2.setBounds(460, 123, 137, 24);
+																				
+																				SettingsPanel.add(comboBoxPar2);
+																				comboBoxStopBits2.setModel(new DefaultComboBoxModel(new String[] {"1", "1.5", "2"}));
+																				comboBoxStopBits2.setBounds(460, 162, 137, 24);
+																				
+																				SettingsPanel.add(comboBoxStopBits2);
+																				
+																				JLabel lblNewLabel_5 = new JLabel("DA5XXX");
+																				lblNewLabel_5.setBounds(483, 9, 70, 15);
+																				SettingsPanel.add(lblNewLabel_5);
+																				
+																				windowChartWidth = new JSpinner();
+																				windowChartWidth.setModel(new SpinnerNumberModel(new Integer(100), new Integer(0), null, new Integer(1)));
+																				windowChartWidth.setBounds(170, 349, 75, 24);
+																				SettingsPanel.add(windowChartWidth);
+																				
+																				JLabel lblNewLabel_6 = new JLabel("chart width (dots)");
+																				lblNewLabel_6.setBounds(34, 353, 132, 15);
+																				SettingsPanel.add(lblNewLabel_6);
 		frame.getContentPane().setLayout(groupLayout);
 
 		JMenuBar menuBar = new JMenuBar();
@@ -378,6 +407,10 @@ public class PenFormView {
 						} catch (Exception e2) {
 							// TODO Auto-generated catch block
 							e2.printStackTrace();
+							if (null != paramsCom0) conCom0.close();
+							paramsCom0 = null;
+							isSimulation = 1;
+
 							return;
 						}
 						System.out.println("Get DA-555 id");
@@ -387,20 +420,33 @@ public class PenFormView {
 						trans = new ModbusSerialTransaction(conCom0);
 						trans.setRequest(req);
 						try {
-							trans.execute();
+							trans.execute(); 
+
 						} catch (ModbusIOException e1) {
 							// TODO Auto-generated catch block
-							e1.printStackTrace();
-							return;
+							e1.printStackTrace();	
+							if (null != paramsCom0) conCom0.close();
+							paramsCom0 = null;
+							isSimulation = 1;
+							//return;
 						} catch (ModbusSlaveException e1) {
 							// TODO Auto-generated catch block
 							e1.printStackTrace();
-							return;
+							if (null != paramsCom0) conCom0.close();
+							paramsCom0 = null;
+							isSimulation = 1;
+							//return;
+
 						} catch (ModbusException e1) {
 							// TODO Auto-generated catch block
 							e1.printStackTrace();
-							return;
+							if (null != paramsCom0) conCom0.close();
+							paramsCom0 = null;
+							isSimulation = 1;
+							//return;
+
 						}
+						if(paramsCom0 != null){
 						rres = trans.getResponse();
 						InputRegister[] registers = new Register[50];
 						registers = rres.getRegisters();
@@ -414,6 +460,7 @@ public class PenFormView {
 								+ registers[1].toBytes()[1]);// PrgrmMounth:=ComBufRX[6];
 						System.out.println("PrgrmYear   = "
 								+ (2000 + registers[2].toBytes()[0]));// PrgrmYear:=2000+ComBufRX[7];
+						}
 					}
 					btnStartStop.setText("Starting... Stop?");
 					myTimer.start();
@@ -428,8 +475,12 @@ public class PenFormView {
 		});
 		String[] ports = SerialPortList.getPortNames();
 		for (String port : ports) {
+			comboBoxPort2.addItem(port);
 			comboBoxPort1.addItem(port);
 			comboBoxPort0.addItem(port);
 		}
+//		comboBoxPort0.setSelectedIndex(0);
+//		comboBoxPort1.setSelectedIndex(1);
+//		comboBoxPort2.setSelectedIndex(2);
 	}
 }
